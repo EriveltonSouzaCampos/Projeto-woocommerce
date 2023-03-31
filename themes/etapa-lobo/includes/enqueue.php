8171e0ae16d8ff3eb8d9d1cb735cc20836cbf42b
Erivelton de Souza Campos <ercampos@id.uff.br>
2023-03-31 18:54:42 -0300

    feat: arquivando e enfileirando o arq de estilo

--- themes/etapa-lobo/includes/enqueue.php
+++ themes/etapa-lobo/includes/enqueue.php
@@ -6,14 +6,15 @@ function estilos_tema(){
     wp_register_style('style_Lobo_reset', STYLES_DIR . '/reset.css', [], '1.0.0', false);
     wp_register_style('style', get_stylesheet_uri(), [], '1.0.0', false);
     wp_register_style('style-home', STYLES_DIR . '/style-home.css', [], '1.0.0', false);
+    wp_register_style('style-header', STYLES_DIR . '/style-header.css', [], '1.0.0', false);
+
 
     
     //Enfileirando
     wp_enqueue_style('style_Lobo_reset');
     wp_enqueue_style('style');
     wp_enqueue_style('style-home');
-
-   
+    wp_enqueue_style('style-header');
 
     
 }
