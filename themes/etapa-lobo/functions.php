2a71a130065027f4899012a0f91d5f6050288b38
Erivelton de Souza Campos <ercampos@id.uff.br>
2023-03-31 16:59:15 -0300

    feat: add arquivos ao tema

---
+++ themes/etapa-lobo/functions.php
@@ -0,0 +1,17 @@
+<?php 
+//varaiveis
+define('ROOT_DIR', get_theme_file_path());
+define('STYLES_DIR', get_template_directory_uri() . '/assets/css');
+define('IMAGE_DIR', get_template_directory_uri() . '/assets/images');
+define('INCLUDES_DIR', ROOT_DIR . '/includes');
+define('JAVA_DIR', get_template_directory_uri() . '/assets/js');
+
+//Includes
+
+include_once(INCLUDES_DIR . '/enqueue.php');
+/* include_once(INCLUDES_DIR . '/setup-theme.php'); */
+
+ //hooks
+ add_action('wp_enqueue_scripts', 'estilos_tema');
+
+?>
