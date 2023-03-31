232c642bf5c7561f682ded7581b359642af88b07
Erivelton de Souza Campos <ercampos@id.uff.br>
2023-03-31 17:08:17 -0300

    iniciando repositorio

---
+++ themes/etapa-lobo/includes/enqueue.php
@@ -0,0 +1,22 @@
+<?php 
+
+function estilos_tema(){
+    
+    //Declarando qual o arquivo (registrando)
+    wp_register_style('style_Lobo_reset', STYLES_DIR . '/reset.css', [], '1.0.0', false);
+    wp_register_style('style', get_stylesheet_uri(), [], '1.0.0', false);
+    wp_register_style('style-home', STYLES_DIR . '/style-home.css', [], '1.0.0', false);
+
+    
+    //Enfileirando
+    wp_enqueue_style('style_Lobo_reset');
+    wp_enqueue_style('style');
+    wp_enqueue_style('style-home');
+
+   
+
+    
+}
+
+
+?>
