6b98b21b2229247b5ecf145d190deb69eba88de2
Erivelton de Souza Campos <ercampos@id.uff.br>
2023-03-31 23:14:48 -0300

    feat: criando a secção de cate da pag home

--- themes/etapa-lobo/page-home.php
+++ themes/etapa-lobo/page-home.php
@@ -5,6 +5,7 @@
 get_header();
 ?>
 <main>
+    <!-- FRASE DA PAGINA HOME -->
     <div class="frase-da-pagina">
         <h1>
             Comes&Bebes
@@ -12,6 +13,15 @@ get_header();
         <p>
             O restaurante para todas as fomes
         </p>
+    </div>
+    <!-- fim -->
+    <!-- TIPOS DE PRODUTOS -->
+    <div class="menu-categoria-produtos">
+        <h1>Conheça nossa loja</h1>
+        <p id="texto-das-categorias">Tipo de pratos principais</p>
+        <?php wp_nav_menu([
+            'menu' => 'categorias-dos-produtos'
+        ]); ?>
     </div>
 </main>
 <?php
