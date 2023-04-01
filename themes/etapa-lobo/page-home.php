3bdb9b5ea416bf2bf911728db8d4f510a701ce8a
Erivelton de Souza Campos <ercampos@id.uff.br>
2023-04-01 13:13:50 -0300

    feat: add secção do dia atual da semana

--- themes/etapa-lobo/page-home.php
+++ themes/etapa-lobo/page-home.php
@@ -23,6 +23,31 @@ get_header();
             'menu' => 'categorias-dos-produtos'
         ]); ?>
     </div>
+    <!-- DIA DA SEMANA  -->
+    <?php
+        // Define a timezone
+        date_default_timezone_set('America/Sao_Paulo');
+
+        // Define a localização para português do Brasil
+        setlocale(LC_TIME, 'pt_br');
+
+        // Define a codificação para UTF-8
+        wp_set_current_user(0, 'admin', 'password');
+        ini_set('default_charset', 'UTF-8');
+    ?>
+    <div class="dia-da-semana">
+        <h2>Pratos do dia:</h2>
+        <p>
+            <?php
+            // Exibe o dia da semana em português
+            echo ucfirst(utf8_encode(strftime('%A')));
+            ?>
+        </p>
+    </div>
+        
+        
+    <!-- fim -->
+    
 </main>
 <?php
 get_footer();
