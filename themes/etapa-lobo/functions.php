6a44d6e9670c1b2b9ec4f4cfb6baddf060e00dae
Erivelton de Souza Campos <ercampos@id.uff.br>
2023-03-31 23:14:12 -0300

    feat: add filtro para modificar o menus

--- themes/etapa-lobo/functions.php
+++ themes/etapa-lobo/functions.php
@@ -15,4 +15,29 @@ include_once(INCLUDES_DIR . '/setup-theme.php');
  add_action('wp_enqueue_scripts', 'estilos_tema');
  add_action('after_setup_theme', 'setup_theme_pf');
 
+ /* Edita o nav menu para aparecer estes elementos*/
+ add_filter('wp_nav_menu_objects', 'transforma_menu');
+ function transforma_menu($itens){
+    foreach($itens as $item){
+      $thumbnail_id = get_term_meta($item->object_id, 'thumbnail_id', true);
+      $image = wp_get_attachment_url($thumbnail_id);
+      $css = 'background-image: url('. $image .')';
+      $item->title = 
+
+    '
+    <div class=conter-cate>
+    <div class="Categoria-content" style="'. $css .';">
+    <div class="Categoria-titulo">
+        <p class="Title">'.$item->title.'</p>
+    </div>
+    </div>
+    
+    </div>';
+    }
+    return $itens;
+  }
+  add_filter( 'woocommerce_shortcode_products_query', 'woocommerce_shortcode_products_orderby' );
+
+/*fim*/
+
 ?>
